cec712dc0c1f4c627894f9c0cc49ca53f588ef99
Sebastian Meisel <sebastian.meisel@gmail.com>
2025-01-25 20:54:01 +0100

    Added TiB and PiB to unit.sh

--- units.sh
+++ units.sh
@@ -1,12 +1,20 @@
 function KiB() {
 	bytes=$1
-	echo "$1 * 2^10" | bc
+	printf "%s B\n" $(echo "$1 * 2^10" | bc)
 }
 function MiB() {
 	bytes=$1
-	echo "$1 * 2^20" | bc
+	printf "%s B\n" $(echo "$1 * 2^20" | bc)
 }
 function GiB() {
 	bytes=$1
-	echo "$1 * 2^30" | bc
+	printf "%s B\n" $(echo "$1 * 2^30" | bc)
+}
+function TiB() {
+	bytes=$1
+	printf "%s B\n" $(echo "$1 * 2^40" | bc)
+}
+function PiB() {
+	bytes=$1
+	printf "%s B\n" $(echo "$1 * 2^50" | bc)
 }
